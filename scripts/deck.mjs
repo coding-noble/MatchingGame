90b0c6c5904f4807eed70b479acf3550ce80895b
Miles Noble <105387328+coding-noble@users.noreply.github.com>
2023-10-20 17:05:31 -0600

    Weekly Changes

--- scripts/deck.mjs
+++ scripts/deck.mjs
@@ -1,17 +1,36 @@
 import { Card } from "./card.mjs";
 
 export class Deck {
-    constructor() {
+    constructor(difficulty) {
         this.cards = [];
-        this.initializeDeck();
+        if (difficulty == "hard") {
+            this.cardCount = 12;            
+        } else if (difficulty == "normal") {
+            this.cardCount = 8;
+        } else {
+            this.cardCount = 4;
+        }
     }
 
-    initializeDeck() {
-        fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=52`).then(response => response.json()).then(cardsData => {
+    async initializeDeck() {
+        try {
+            const response = await fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${this.cardCount}`);
+            const cardsData = await response.json();
+
             cardsData.cards.forEach(card => {
                 let newCard = new Card(card.suit, card.code, card.images.svg);
                 this.cards.push(newCard);
+                this.cards.push(newCard);
             });
-        }).catch(error => console.error('Error fetching cards from API:', error));
+
+            // Fisher-Yates shuffle or Knuth shuffle
+            for (let i = this.cards.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+            }
+
+        } catch (error) {
+            console.error('Error fetching cards from API:', error);
+        }
     }
 }
